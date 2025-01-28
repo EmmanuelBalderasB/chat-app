ea22544144d52e00df21cb476044bfdaf7a933c0
Emmanuel <ebalderasbeq@gmail.com>
2025-01-28 03:07:59 -0600

    changes width of chat responses

--- src/Chat/SystemMessage.tsx
+++ src/Chat/SystemMessage.tsx
@@ -10,7 +10,7 @@ const SystemMessage: React.FC<SystemMessageProps> = ({ text, timestamp }) => {
 			<div className="message-timestamp text-sm text-zinc-500">
 				{timestamp}
 			</div>
-			<div className="max-w-32 text-zinc-200 px-4 py-2 rounded-lg">
+			<div className="max-w-2/3 text-zinc-200 px-4 py-2 rounded-lg">
 				<div className="message-text">{text}</div>
 			</div>
 		</div>
