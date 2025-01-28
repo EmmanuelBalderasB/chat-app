423f17fe1aa35c59e59235900f36864d1c67d83d
Emmanuel <ebalderasbeq@gmail.com>
2025-01-28 02:17:58 -0600

    adds system message component template

--- src/Chat/SystemMessage.tsx
+++ src/Chat/SystemMessage.tsx
@@ -5,7 +5,16 @@ interface SystemMessageProps {
 
 const SystemMessage: React.FC<SystemMessageProps> = ({ text, timestamp }) => {
 	console.log(text, timestamp)
-	return <></>
+	return (
+		<div className="message-container flex flex-col items-start mb-3">
+			<div className="message-timestamp text-sm text-zinc-500">
+				{timestamp}
+			</div>
+			<div className="max-w-32 text-zinc-200 px-4 py-2 rounded-lg">
+				<div className="message-text">{text}</div>
+			</div>
+		</div>
+	)
 }
 
 export default SystemMessage
